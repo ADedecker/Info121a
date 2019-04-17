7ef3f25a58a5076c31656609b574394b439c3938
Gad3lan <alan.adamiak@gmail.com>
2019-04-17 15:23:15 +0200

    TP5 ex1

---
+++ TP5/arbre.cpp
@@ -0,0 +1,209 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+struct branch {
+	int data;
+	branch *left;
+	branch *right;
+	int visited;
+};
+
+struct tree {
+	branch *root;
+};
+
+branch *newBranch(int val) {
+	branch *b = (branch*) malloc (sizeof(branch));
+	b -> data = val;
+	b -> visited = 0;
+	b -> left = NULL;
+	b -> right = NULL;
+	return b;
+}
+
+void showRec(branch *b, int space) {
+	for (int i = 0; i < space; i++) {
+		if (i < space - 1)
+			cout << "  ";
+		else
+			cout << "\u21B3 ";
+	}
+	cout << b -> data << endl;
+	if (b -> left != NULL) {
+		showRec(b -> left, space+1);
+	}
+	if (b -> right != NULL) {
+		showRec(b -> right, space+1);
+	}
+}
+
+void showTree(tree *t) {
+	branch *b = t -> root;
+	showRec(b, 0);
+}
+
+void addValIter(tree *t, int val) {
+	branch *b = t -> root;
+	while(b -> left != NULL || b -> right != NULL) {
+		if (val >= b -> data) {
+			b = b -> right;
+		} else {
+			b = b -> left;
+		}
+	}
+	if (val >= b -> data) {
+		b -> right = newBranch(val);
+	} else {
+		b -> left = newBranch(val);
+	}
+}
+
+void recAddVal(branch *b, int val) {
+	if (val >= b -> data && b ->right != NULL)
+		recAddVal(b -> right, val);
+	else if (val < b -> data && b ->left != NULL)
+		recAddVal(b -> left, val);
+	if (b ->left == NULL || b ->right == NULL) {
+		if (val >= b -> data) {
+			b -> right = newBranch(val);
+		} else {
+			b -> left = newBranch(val);
+		}
+	}
+}
+
+void addValRec(tree *t, int val) {
+	recAddVal(t -> root, val);
+}
+
+int searchMin(tree *t) {
+	branch *b = t -> root;
+	while (b -> left == NULL) {
+		b = b -> left;
+	}
+	return b -> data;
+}
+
+int recDeepness(branch *b) {
+	if (b -> left != NULL && b -> right != NULL) {
+		return 1 + max(recDeepness(b -> left), recDeepness(b -> right));
+	} else if (b -> right != NULL) {
+		return 1 + recDeepness(b ->right);
+	} else if (b -> left != NULL) {
+		return 1 + recDeepness(b -> left);
+	} else {
+		return 1;
+	}
+}
+
+int deepnessRec(tree *t) {
+	return recDeepness(t -> root);
+}
+
+int deepnessIter(tree *t) {
+	branch *b = t -> root;
+	int max = 0;
+	int nb = 0;
+	while (b && !b -> visited) {
+		if (b -> left && !b -> left->visited) {
+			nb++;
+			b = b -> left;
+		} else if (b -> right && !b -> right->visited) {
+			nb++;
+			b = b -> right;
+		} else {
+			nb++;
+			if (nb > max)
+				max = nb;
+			nb = 0;
+			b -> visited = 1;
+			b = t -> root;
+		}
+	}
+	return max;
+}
+
+int recContains(branch *b, int val) {
+	if (b -> data != val) {
+		if (val < b -> data && b -> left != NULL) {
+			return recContains(b -> left, val);
+		}
+		else if (val >= b -> data && b -> right != NULL) {
+			return recContains(b -> right, val);
+		}
+		else {
+			return 0;
+		}
+	} else {
+		return 1;
+	}
+}
+
+int contains(tree *t, int val) {
+	return recContains(t -> root, val);
+}
+
+branch *findMin(branch *b) {
+	while (b -> left != NULL) {
+		b = b -> left;
+	}
+	return b;
+}
+
+branch* recDel(branch *b, int val) {
+	if (b == NULL)
+		return NULL;
+	if (val < b -> data) {  // data is in the left sub tree.
+		b->left = recDel(b->left, val);
+	} else if (val > b->data) { // data is in the right sub tree.
+		b->right = recDel(b->right, val);
+	} else {
+		// case 1: no children
+		if (b->left == NULL && b->right == NULL) {
+			delete(b); // wipe out the memory, in C, use free function
+			b = NULL;
+		}
+		// case 2: one child (right)
+		else if (b->left == NULL) {
+			branch *temp = b; // save current node as a backup
+			b = b->right;
+			delete temp;
+		}
+		// case 3: one child (left)
+		else if (b->right == NULL) {
+			branch *temp = b; // save current node as a backup
+			b = b->left;
+			delete temp;
+		}
+		// case 4: two children
+		else {
+			branch *temp = findMin(b -> right); // find minimal value of right sub tree
+			b->data = temp->data; // duplicate the node
+			b->right = recDel(b->right, temp->data); // delete the duplicate node
+		}
+	}
+	return b;
+}
+
+void del(tree *t, int val) {
+	recDel(t -> root, val);
+}
+
+int main() {
+	tree *t = (tree*) malloc (sizeof(tree));
+	t -> root = newBranch(5);
+	t -> root -> left = newBranch(4);
+	t -> root -> right = newBranch(6);
+	t -> root -> left -> left = newBranch(3);
+	addValRec(t, 4);
+	addValRec(t, 4);
+	addValIter(t, 3);
+	cout << "Profondeur recursive: " << deepnessRec(t) << endl;
+	cout << "Profondeur iterative: " << deepnessIter(t) << endl;
+	showTree(t);
+	del(t, 6);
+	showTree(t);
+	return 0;
+}
